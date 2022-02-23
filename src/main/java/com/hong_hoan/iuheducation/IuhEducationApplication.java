7100c7a050ff14cd9288ce5ad000391f80ca2a4c
Hoan Truong <hoantruong6814@gamil.com>
2022-02-23 21:13:15 +0700

    create source

---
+++ src/main/java/com/hong_hoan/iuheducation/IuhEducationApplication.java
@@ -0,0 +1,13 @@
+package com.hong_hoan.iuheducation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IuhEducationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IuhEducationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hong_hoan/iuheducation/IuhEducationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hong_hoan.iuheducation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IuhEducationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
