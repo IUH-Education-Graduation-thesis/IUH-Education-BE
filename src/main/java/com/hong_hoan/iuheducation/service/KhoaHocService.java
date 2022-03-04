35d56b68866f7970e3ad27c7afcd64ee28d660c6
Hoan Truong <hoantruong6814@gamil.com>
2022-03-04 16:09:51 +0700

    done api find  khoa hoc

--- src/main/java/com/hong_hoan/iuheducation/service/KhoaHocService.java
+++ src/main/java/com/hong_hoan/iuheducation/service/KhoaHocService.java
@@ -8,11 +8,32 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.NoSuchElementException;
+
 @Service
 @RequiredArgsConstructor
 public class KhoaHocService {
     @Autowired
     private KhoaRepository khoaRepository;
+
+    public List<Khoa> findAllKhoaHoc() {
+        List<Khoa> _khoa = khoaRepository.findAll();
+
+        return _khoa;
+    }
+
+    public Khoa findById(String id) throws NumberFormatException {
+        long _id = Long.valueOf(id);
+
+        try {
+            Khoa _khoa = khoaRepository.findById(_id).get();
+
+            return _khoa;
+        } catch (NoSuchElementException ex) {
+            throw new KhoaHocIsNotExist();
+        }
+    }
 
     public void deleteKhoaHoc(String id) throws NumberFormatException {
         long _idLong = Long.valueOf(id);
