893718f99f4d499903a8e7c5db287504d7e7e609
Hoan Truong <hoantruong6814@gamil.com>
2022-03-05 00:12:14 +0700

    build api get profile

--- src/main/java/com/hong_hoan/iuheducation/resolvers/response/ProfileResponse.java
+++ src/main/java/com/hong_hoan/iuheducation/resolvers/response/ProfileResponse.java
@@ -1,2 +1,22 @@
-package com.hong_hoan.iuheducation.resolvers.response;public class ProfileResponse {
+package com.hong_hoan.iuheducation.resolvers.response;
+
+import com.hong_hoan.iuheducation.entity.Account;
+import com.hong_hoan.iuheducation.entity.NamHoc;
+import com.hong_hoan.iuheducation.resolvers.common.ErrorResponse;
+import com.hong_hoan.iuheducation.resolvers.common.ResponseInterface;
+import com.hong_hoan.iuheducation.resolvers.common.ResponseStatus;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.util.List;
+
+@Builder
+@Getter
+@ToString
+public class ProfileResponse implements ResponseInterface {
+    private ResponseStatus status;
+    private List<ErrorResponse> errors;
+    private String message;
+    private List<Account> data;
 }
