4c5118323702abed89211dd0c59688af276e8d33
dtthong31 <hongblack2000@gmail.com>
2022-03-23 21:30:37 +0700

    sua api

---
+++ src/main/java/com/hong_hoan/iuheducation/repository/LichHocRepository.java
@@ -0,0 +1,7 @@
+package com.hong_hoan.iuheducation.repository;
+
+import com.hong_hoan.iuheducation.entity.LichHoc;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LichHocRepository extends JpaRepository<LichHoc, Long> {
+}
---
+++ src/main/java/com/hong_hoan/iuheducation/resolvers/input/lich_hoc/ThemLichHocInputs.java
@@ -0,0 +1,24 @@
+package com.hong_hoan.iuheducation.resolvers.input.lich_hoc;
+
+import com.hong_hoan.iuheducation.entity.LopHocPhan;
+import com.hong_hoan.iuheducation.entity.PhongHoc;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+import java.util.Date;
+
+@Builder
+@Getter
+@ToString
+public class ThemLichHocInputs {
+    private long id;
+    private int ngayHocTrongTuan;
+    private int nhomThucHanh;
+    private Date thoiGianBatDau;
+    private Date thoiGianKetThuc;
+    private int tietHocBatDau;
+    private int tietHocKetThuc;
+    private String ghiChu;
+    private PhongHoc phongHoc;
+    private LopHocPhan lopHocPhan;
+}
